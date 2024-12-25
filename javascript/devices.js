4b2b379d17f9309863bde99fceb9748c18d212af
Roben Kleene <contact@robenkleene.com>
2024-12-25 17:44:21 -0500

    Work on hacking together js

--- javascript/devices.js
+++ javascript/devices.js
@@ -1,17 +1,29 @@
-var trackAPI = null; // LiveAPI object for the track
-var devices = []; // Array to store device IDs
-var deviceNames = []; // Array to store device names
+var trackAPI = null;
+var devices = [];
+var deviceNames = [];
 
-// Function to handle input in the left inlet (track ID)
+TRACK_INLET = 0;
+INDEX_INLET = 1;
+
+// Inlets & Outlets
+inlets = 2;
+outlets = 2;
+
+function msg_int(value) {
+  if (inlet == TRACK_INLET)
+  values[inlet] = value;
+  outlet(inlet, "int " + value);
+}
+
+
+
 function inlet1(trackID) {
     if (trackAPI) {
-        trackAPI.property = ""; // Detach observer from the old track
+        trackAPI.property = "";
     }
 
-    // Set up a new LiveAPI object for the given track ID
     trackAPI = new LiveAPI(trackID);
 
-    // Check if the track has devices
     if (trackAPI.getcount("devices") > 0) {
         updateDevices();
     } else {
@@ -21,40 +33,26 @@ function inlet1(trackID) {
     }
 }
 
-// Function to update the list of devices
 function updateDevices() {
-    // Clear the current lists
     devices = [];
     deviceNames = [];
 
-    // Get the number of devices in the track
     var deviceCount = trackAPI.getcount("devices");
 
-    // Iterate through devices
     for (var i = 0; i < deviceCount; i++) {
         var devicePath = trackAPI.path + " devices " + i;
         var deviceAPI = new LiveAPI(devicePath);
-
-        // Store the device ID and name
         devices.push(deviceAPI.id);
         deviceNames.push(deviceAPI.get("name"));
     }
 
-    // Output the device names to the left outlet
     outlet(0, deviceNames);
 }
 
-// Function to handle input in the right inlet (index value)
 function inlet2(index) {
     if (index >= 0 && index < devices.length) {
-        // Output the device ID to the right outlet
         outlet(1, devices[index]);
     } else {
         outlet(1, "Invalid index");
     }
-}
-
-// Initialization
-function bang() {
-    post("track_devices.js loaded\n");
 }
