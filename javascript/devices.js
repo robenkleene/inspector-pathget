03f27ddd14156a6611fdcebefd28872147ff626a
Roben Kleene <contact@robenkleene.com>
2024-12-25 17:53:36 -0500

    Work on devices

--- javascript/devices.js
+++ javascript/devices.js
@@ -1,4 +1,3 @@
-var trackAPI = null;
 var devices = [];
 var deviceNames = [];
 
@@ -10,43 +9,31 @@ inlets = 2;
 outlets = 2;
 
 function msg_int(value) {
-  if (inlet == TRACK_INLET)
-  values[inlet] = value;
-  outlet(inlet, "int " + value);
+  switch (inlet) {
+  case TRACK_INLET:
+    processTrack(value);
+    break;
+  case INDEX_INLET:
+    processIndex(value);
+    break;
+  default:
+    break;
+  }
 }
 
+function processTrack(id) {
+  trackAPI = new LiveAPI(id);
+  devices = [];
+  deviceNames = [];
 
-
-function inlet1(trackID) {
-    if (trackAPI) {
-        trackAPI.property = "";
-    }
-
-    trackAPI = new LiveAPI(trackID);
-
-    if (trackAPI.getcount("devices") > 0) {
-        updateDevices();
-    } else {
-        devices = [];
-        deviceNames = [];
-        outlet(0, "No devices found");
-    }
-}
-
-function updateDevices() {
-    devices = [];
-    deviceNames = [];
-
-    var deviceCount = trackAPI.getcount("devices");
-
-    for (var i = 0; i < deviceCount; i++) {
-        var devicePath = trackAPI.path + " devices " + i;
-        var deviceAPI = new LiveAPI(devicePath);
-        devices.push(deviceAPI.id);
-        deviceNames.push(deviceAPI.get("name"));
-    }
-
-    outlet(0, deviceNames);
+  var deviceCount = trackAPI.getcount("devices");
+  for (var i = 0; i < deviceCount; i++) {
+    var devicePath = trackAPI.path + " devices " + i;
+    var deviceAPI = new LiveAPI(devicePath);
+    devices.push(deviceAPI.id);
+    deviceNames.push(deviceAPI.get("name"));
+  }
+  outlet(0, deviceNames);
 }
 
 function inlet2(index) {
